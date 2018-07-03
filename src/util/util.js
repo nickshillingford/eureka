1b624d56f647386165500f520e0400ee957dd4be
Nick Shillingford <nshillingford21@gmail.com>
2018-07-03 12:46:34 -0400

    Update util.js

--- src/util/util.js
+++ src/util/util.js
@@ -15,54 +15,12 @@ let easeFunctions = {
   'circ-ease-in-out': Circ.easeInOut
 };
 
-function sumVectors(v1, v2, ops) {
-  var options = {};
-
-  if (ops != undefined) {
-    options = ops;
-  }
-
-  let svg = document.querySelector('svg');
-  let sumX = v1.ipc[0] + v2.ipc[0];
-  let sumY = v1.ipc[1] + v2.ipc[1];
-  let sum = new Vector([sumX, sumY], options);
-
-  if (sum.ops.showCoordinates) {
-    svg.append(sum.coordLabel);
-  }
-
-  if ((ops != undefined) && (ops.animate == false)) {
-     svg.append(sum.arrow);
-  }
-  else {
-     let def = { ease: 'linear', duration: 1, delay: 0 };
-     let tl = new TimelineMax();
-
-     if ((ops != undefined) && ops.ease) {
-       def['ease'] = ops['ease'];
-     }
-     if ((ops != undefined) && ops.duration) {
-       def['duration'] = ops['duration'];
-     }
-     if ((ops != undefined) && ops.delay) {
-       def['delay'] = ops['delay'];
-     }
-
-     var c2 = function() {
-       tl.to(v2.arrow, def.duration, { attr: { x1: 600, y1: 350, x2: v2.coord.x2, y2: v2.coord.y2  }, delay: def.delay, ease: easeFunctions[def.ease] });
-     };
-     var c1 = function() {
-       svg.append(sum.arrow);
-       tl.from(sum.arrow, def.duration, { attr: { x2: 600, y2: 350 }, delay: def.delay, ease: easeFunctions[def.ease], onComplete: c2 });
-     };
-     tl.to(v2.arrow, def.duration, { attr: { x1: v1.coord.x2, y1: v1.coord.y2, x2: sum.coord.x2, y2: sum.coord.y2  },
-     delay: def.delay, ease: easeFunctions[def.ease], onComplete: c1 });
-  }
-}
-
 function multiply(m1, m2) {
   let x = ((m1[0][0] * m2[0]) + (m1[0][1] * m2[1]));
   let y = ((m1[1][0] * m2[0]) + (m1[1][1] * m2[1]));
-
   return [x, y];
+}
+
+function determinant(m) {
+  return ((m[1][1] * m[0][0]) - (m[1][0] * m[0][1]));
 }
